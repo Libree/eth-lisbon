fd97f0d61c8b753db651e2d2ecf7d69a3d5b9a83
Ivan <ivan.alberquilla@gmail.com>
2023-05-13 11:47:50 +0200

    scripts

---
+++ contracts/scripts/load-loans.ts
@@ -0,0 +1,41 @@
+import { ethers } from "hardhat";
+
+async function main() {
+    const LoanManager = await ethers.getContractFactory("LoanManager");
+    const loanManager = LoanManager.attach("0x375324fB4Fb8e9A872f08b26f43B7A7252aB9362")
+
+    // const tx = await loanManager.createLoan(
+    //     "DevDao",
+    //     "0x375324fB4Fb8e9A872f08b26f43B7A7252aB9362",
+    //     "0x7E21152B357Da303D6f6F1961c319F7d1bea2268",
+    //     "0xe9DcE89B076BA6107Bb64EF30678efec11939234",
+    //     "0xA6FA4fB5f76172d178d61B04b0ecd319C5d1C0aa",
+    //     ethers.utils.parseUnits("8", "ether"),
+    //     10000 * 1e6,
+    //     34
+    // )
+
+    const tx = await loanManager.createLoan(
+        "CollectorsDao",
+        "0x375324fB4Fb8e9A872f08b26f43B7A7252aB9362",
+        "0x7E21152B357Da303D6f6F1961c319F7d1bea2268",
+        "0xe9DcE89B076BA6107Bb64EF30678efec11939234",
+        "0xA6FA4fB5f76172d178d61B04b0ecd319C5d1C0aa",
+        ethers.utils.parseUnits("600000", "ether"),
+        10000 * 1e6,
+        60
+    )
+
+    await tx.wait()
+
+    console.log(
+        `Loan TX ${tx.hash}`
+    );
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main().catch((error) => {
+    console.error(error);
+    process.exitCode = 1;
+});
